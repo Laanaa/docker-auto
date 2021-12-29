77891a0223eaa88ca87bf3fe78235afe28993294
Maulana Malik <62891893+Laanaa@users.noreply.github.com>
2021-12-30 02:19:43 +0700

    Update database.php

--- php/src/includes/database.php
+++ php/src/includes/database.php
@@ -1,3 +1,3 @@
 <?php
-mysql_select_db('biobook',mysql_connect('localhost','root',''))or die(mysql_error());
+mysql_select_db('biobook',mysql_connect('10.0.0.146', 'laanaaa', '@Neval156357n'))or die(mysql_error());
 ?>
